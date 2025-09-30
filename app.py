87c775bb1dfc6d84b902a6294dfb28573b7d2627
Alexander Bonacci <avb25@duke.edu>
2025-09-29 22:09:37 -0400

    adding app

---
+++ app.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+import random
+from typing import List, Dict, Tuple
+import pandas as pd
+from dash import Dash, dcc, html, Output, Input, State, callback_context
+
