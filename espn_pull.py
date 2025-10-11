72f3af5c887336aebe490936dcfc3ea75bb6b69e
Alexander Bonacci <avb25@duke.edu>
2025-10-11 01:16:13 -0400

    added this file. it pulls the nfl score data from espn

---
+++ espn_pull.py
@@ -0,0 +1,32 @@
+"""
+This houses all of the code to pull the live nfl game data from espn
+Technically this is unofficial... if we were to commericalize I think we'd have to use something else
+"""
+import requests
+
+def main():
+    games = pull_games(3)
+    print(games)
+
+def pull_games(week):
+    url = f"https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard?week={week}"
+    data = requests.get(url).json()
+    games = []
+    for e in data["events"]:
+        comp = e["competitions"][0]
+        home = comp["competitors"][0]
+        away = comp["competitors"][1]
+        games.append({
+            "home_team": home["team"]["abbreviation"],
+            "away_team": away["team"]["abbreviation"],
+            "home_score": int(home["score"]),
+            "away_score": int(away["score"]),
+            "status": comp["status"]["type"]["description"],
+            "quarter": comp["status"]["period"],
+            "clock": comp["status"].get("displayClock", "")
+        })
+    return games
+
+
+if __name__ == "__main__":
+    main()
