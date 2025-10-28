50c666fc7e6358780105cc9fdec943a4866c88c0
Alexander Bonacci <avb25@duke.edu>
2025-10-28 12:27:47 -0400

    added to readme and also starting to figure out how to get the live lines from espn

--- espn_pull.py
+++ espn_pull.py
@@ -5,8 +5,9 @@ Technically this is unofficial... if we were to commericalize I think we'd have 
 import requests
 
 def main():
-    games = pull_games(3)
-    print(games)
+    # games = pull_games(3)
+    # print(games)
+    pull_lines()
 
 def pull_games(week):
     url = f"https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard?week={week}"
@@ -27,6 +28,18 @@ def pull_games(week):
         })
     return games
 
+def pull_lines():
+    resp = requests.get("https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard")
+    data = resp.json()
+
+    for event in data["events"]:
+        print(event)
+        comp = event["competitions"][0]
+        if "odds" in comp:
+            odds = comp["odds"][0]
+            home = comp["competitors"][0]["team"]["abbreviation"]
+            away = comp["competitors"][1]["team"]["abbreviation"]
+            print(home, away, odds["details"])
 
 if __name__ == "__main__":
     main()
